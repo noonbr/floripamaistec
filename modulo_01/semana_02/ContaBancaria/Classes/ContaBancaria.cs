bdf8b407cce37bad1b51e45a2b89c48af2dec51b
Noon <nonpl4ys@gmail.com>
2023-02-11 21:44:32 -0300

    Adicionado os exercícos da semana 02

---
+++ modulo_01/semana_02/ContaBancaria/Classes/ContaBancaria.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContaBancaria.Classes
+{
+    public class cBancaria
+    {
+        public int numero;
+        public int agencia;
+        public string titular;
+        public int saldo = 0;
+        public tipoContaBancaria tipo;
+
+        public void depositar(int novoSaldo)
+        {
+            if ((int) novoSaldo > 0)
+            {
+                this.saldo += novoSaldo;
+            };
+        }
+
+        public void sacar(int qtdSaque)
+        {
+            if (qtdSaque > 0 && qtdSaque <= this.saldo)
+            {
+                this.saldo -= qtdSaque;
+            } else
+            {
+                Console.WriteLine("Você não tem saldo suficiente para efeutar o saque.");
+            }
+        }
+
+        public void mostrarSaldo()
+        {
+            Console.WriteLine($"O saldo atual da conta de {this.titular} é de R$ {this.saldo}.");
+        }
+    }
+    
+    public enum tipoContaBancaria
+    {
+        CORRENTE,
+        POUPANCA
+    }
+}
---
+++ modulo_01/semana_02/ContaBancaria/Program.cs
@@ -0,0 +1,10 @@
+﻿using ContaBancaria.Classes;
+
+cBancaria minhaConta = new cBancaria();
+minhaConta.titular = "Jailson";
+minhaConta.numero = 3213441;
+minhaConta.tipo = tipoContaBancaria.POUPANCA;
+
+minhaConta.depositar(1500);
+minhaConta.sacar(135);
+minhaConta.mostrarSaldo();
